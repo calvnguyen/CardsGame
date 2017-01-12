cb3a6ee42fb3eb7a4738006f32456bb313a7bd93
Calvin Nguyen <azcalv@Calvins-MacBook-Air.local>
2017-01-12 11:40:33 -0800

    first commit

--- CardsGame/ViewController.swift
+++ CardsGame/ViewController.swift
@@ -10,11 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Card6: UIImageView!
+    @IBOutlet weak var Card5: UIImageView!
+    @IBOutlet weak var Card4: UIImageView!
+    @IBOutlet weak var Card3: UIImageView!
+    @IBOutlet weak var Card2: UIImageView!
+    @IBOutlet weak var Card1: UIImageView!
+    var newDeck: Deck!
+    var newPlayer: Player!
+    var card_set: [Card] = []
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        newDeck = Deck()
+        newPlayer = Player(name: "Calvin")
+        
+        Card1.isHidden = true
+        Card2.isHidden = true
+        Card3.isHidden = true
+        Card4.isHidden = true
+        Card5.isHidden = true
+        Card6.isHidden = true
+        
     }
 
+    @IBAction func drawButtonClicked(_ sender: UIButton) {
+        if let thiscard = newPlayer.draw(deck: newDeck){
+            newPlayer.hand.append(thiscard)
+        }
+        let image = newPlayer.hand[0].image
+        print(image)
+        
+        Card1.image = UIImage(named: image)
+        Card1.isHidden = false
+        
+    }
+    
+    @IBAction func discardButtonClicked(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func resetButtonClicked(_ sender: UIButton) {
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
